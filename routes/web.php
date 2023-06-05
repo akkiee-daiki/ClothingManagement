ce16bf75b3cb58aa53c2105f81e993cf4c793fe7
akkiee-daiki <daikiakibayashi@aol.com>
2023-06-06 00:14:02 +0900

    refine Route group as (adding "." before brand)

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => '/brand', 'as' => 'brand'], function() {
+Route::group(['prefix' => '/brand', 'as' => 'brand.'], function() {
     Route::get('/', [BrandController::class, 'index'])->name('index');
+    Route::get('/create', [BrandController::class, 'create'])->name('create');
+    Route::match(['get', 'post'], '/create_confirm', [BrandController::class, 'create_confirm'])->name('create_confirm');
+    Route::post('/store', [BrandController::class, 'store'])->name('store');
+    Route::get('/edit', [BrandController::class, 'edit'])->name('edit');
+    Route::match(['get', 'post'], '/edit_confirm', [BrandController::class, 'edit_confirm'])->name('edit_confirm');
+    Route::post('/update', [BrandController::class, 'update'])->name('update');
 });
 
