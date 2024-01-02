b0f2bbfd5e7decf608411767078549d9148f35f7
akkiee-daiki <daikiakibayashi@aol.com>
2024-01-02 11:35:16 +0900

    not complete js deal of destory

--- resources/views/brand/index.blade.php
+++ resources/views/brand/index.blade.php
@@ -4,26 +4,51 @@
 
 @section('main')
 <section id="new">
+    @if(session('complete_msg'))
+        <p>{{ session('complete_msg') }}</p>
+    @endif
     <table class="ta1">
         <thead>
-
+            <th></th>
+            <th>ブランド名</th>
+            <th>日本語名</th>
+            <th></th>
         </thead>
         <tbody>
             @foreach($list ?? [] as $v)
                 <tr>
-                    <th style="width: 10%">{{ $loop->index + 1 }}</th>
+                    <td style="width: 10%">{{ $loop->index + 1 }}</td>
                     <td>{{ $v->name }}</td>
+                    <td>{{ $v->Japanese_name }}</td>
                     <td>
                         <button onclick="location.href='{{ route('brand.edit', $v->brand_id) }}'">編集</button>
-                        <button onclick="location.href='{{ route('brand.create', $v->brand_id) }}'">削除</button>
+                        <button type="button" class="js-destroyBtn" data-id="{{ $v->brand_id }}">削除</button>
                     </td>
                 </tr>
             @endforeach
         </tbody>
 
     </table>
+
+    <form action="{{ route('brand.destroy') }}" class="js-destroyForm">
+        <input type="hidden" name="brand_id" value="">
+    </form>
+
     <div class="btn_container">
         <button onclick="location.href='{{ route('brand.create') }}'">ブランド追加</button>
     </div>
 </section>
+@endsection
+
+@section('js')
+<script>
+    let destroyForm = document.getElementById('js-destroyForm');
+    let destroyBtn = document.getElementsByClassName('js-destroyBtn');
+
+    // get brand_id of clicked delete button
+    $('.js-destroyBtn').addEventListener('click', function () {
+        console.log(this.dataset.id);
+    });
+
+</script>
 @endsection
